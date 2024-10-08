3c770b4009752bf153aed3e69f5a4d286258b426
Jhonatan Leachenski <130028211+jleachenski@users.noreply.github.com>
2024-10-08 09:30:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import post_router from "./routes/post-route.js"
 
 const app = e();
 
+app.get("/", (req, res) => {
+  res.send("servidor rodando")
+           })
+
 app.use(e.json());
 app.use("/user", user_router);
 app.use("/post", post_router);
